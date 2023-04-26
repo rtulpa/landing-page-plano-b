adf5801e182733ce700ea5e5d130cce87c5a0fee
rtulpa <131828815+rtulpa@users.noreply.github.com>
2023-04-26 23:46:28 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var sentences = [
+"no snowflake in an avalanche ever feels responsible",
+"don't be afraid to take that big step",
+"practice non-attachment",
+"be friends with fear",
+"your shadow is your friend",
+"vulnerability and courage are not opposites",
+"don't run away from novelty",
+"learn to love your mistakes",
+"take care of your inner child",
+"do something for you today",
+]
+
+function riskIt() {
+    var randomNumber = Math.floor(Math.random()*sentences.length);
+    document.getElementById('newSentence').innerHTML = sentences[randomNumber];
+}
+
+function sound(){
+    var snd = new Audio('partir.mp3');
+    snd.play();
+}
