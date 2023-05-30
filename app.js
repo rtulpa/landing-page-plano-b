b1ed8bd120de37926fb13d622c3d2164d91cad14
rtulpa <131828815+rtulpa@users.noreply.github.com>
2023-05-30 14:50:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -8,40 +8,26 @@ var sentences = [
     "don't run away from novelty",
     "learn to love your mistakes",
     "take care of your inner child",
-    "do something for you today",
-    ]
-    
-    function riskIt() {
-        var randomNumber = Math.floor(Math.random()*sentences.length);
-        document.getElementById('newSentence').innerHTML = sentences[randomNumber];
-    }
-    
-    function sound(){
-        var snd = new Audio('partir.mp3');
-        snd.play();
-    }
-    
-    function scrollToSlide6() {
-        const slide4 = document.getElementById('slide6');
-        slide4.scrollIntoView({ behavior: 'smooth' });
-      }
-    
-    
-    window.onbeforeunload = function() {
+    "do something for you today"
+  ];
+  
+  function riskIt() {
+    var randomNumber = Math.floor(Math.random() * sentences.length);
+    document.getElementById('newSentence').innerHTML = sentences[randomNumber];
+  }
+  
+  function sound(){
+    var snd = new Audio('partir.mp3');
+    snd.play();
+}
+  
+  
+  function scrollToSlide6() {
+    const slide4 = document.getElementById('slide6');
+    slide4.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-    }
-
-    function sound(){
-        var audio = new Audio("ladyandman.mp3");
-            var isPlaying = false;
-        
-            function toggleMusic() {
-              if (isPlaying) {
-                audio.pause();
-                isPlaying = false;
-              } else {
-                audio.play();
-                isPlaying = true;
-              }
-            }
-    }
+  };
+  
